e1f4e8beb0242649bfc3dd1f7d6c929d543b9cb1
Adarsh29-10 <146656899+Adarsh29-10@users.noreply.github.com>
2024-04-02 22:22:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-var list = document.querySelector(".list");
+var checkBoxs = document.querySelectorAll("input[name='checkBox']");
+var button = document.querySelector("button");
 
-var right = document.querySelector(".right");
+var label = document.querySelectorAll('.item');
 
-list.addEventListener("click", function(){
+
+var labelText = label.textContent;
+
+var right = document.querySelector(".right added-list");
+var value = 0
+checkBoxs.forEach(function(checkbox){
+    value = checkbox.addEventListener("change", function(elem){
+        if(elem.target.checked){
+            console.log("ha bhai")
+        } else{
+            console.log("na bhai")
+        }
+
+        label.forEach(function(label){
+            console.log(label)
+        })
+
+        button.addEventListener("click", function(){
+            // console.log("hello")
+            // right.innerHTML = `<h5> ${list} </h5>`
+
+        })
+    })
     
 })
